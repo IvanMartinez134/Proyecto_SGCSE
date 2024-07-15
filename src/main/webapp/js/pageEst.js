46c4ccf3934e0c1361de91995cca9c447d0143d8
Ivan <20233tn134@utez.edu.mx>
2024-07-15 13:43:56 -0600

    Mas cambios jsp

---
+++ src/main/webapp/js/pageEst.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtener todos los enlaces que deben abrir el modal
+    const openModalLinks = document.querySelectorAll('.open-password-modal');
+
+
+    openModalLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+
+            const modal = document.querySelector('.modal');
+            modal.classList.add('modal--show');
+        });
+    });
+
+
+    const closeModal = document.querySelector('.modal-close');
+
+    closeModal.addEventListener('click', function(e) {
+        e.preventDefault();
+
+
+        const modal = document.querySelector('.modal');
+        modal.classList.remove('modal--show');
+    });
+});
+
+
+
+function togglePassword() {
+    const passwordField = document.getElementById('inputPassword');
+    const passwordFieldType = passwordField.getAttribute('type');
+    const newType = passwordFieldType === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', newType);
+}
+
+
+document.addEventListener('click', function (event) {
+    var target = event.target;
+    if (!target.closest('#user-profile-link') && !target.closest('.user-profile-card')) {
+        var userProfileCard = document.getElementById('user-profile');
+        if (userProfileCard.classList.contains('show')) {
+            userProfileCard.classList.remove('show');
+        }
+    }
+});
+
+
+
+const $openClose = document.getElementById("open_close");
+const $aside = document.getElementById("aside");
+
+$openClose.addEventListener("click", () => {
+    $aside.classList.toggle("abrir");
+});
---
+++ src/main/webapp/js/registroEst.js
@@ -0,0 +1,71 @@
+document.getElementById("registroForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const alerta = document.getElementById("alerta");
+    alerta.textContent = "";
+    alerta.style.color = "red";
+
+    const correo = document.getElementById("correo").value;
+    const contrasena = document.getElementById("contrasena").value;
+    const repetirContrasena = document.getElementById("repetirContrasena").value;
+
+    if (!correo.includes("@")) {
+        alerta.textContent = "*Ingrese una dirección de correo electrónico válida";
+        return;
+    }
+    if (contrasena.length < 8 || !/[A-Z]/.test(contrasena) || !/[a-z]/.test(contrasena) || !/[0-9]/.test(contrasena) || !/[!@#$%^&*(),.?":{}|<>]/.test(contrasena)) {
+        alerta.textContent = "Tu contraseña es insegura. Te recordamos que tiene que tener mínimo 8 caracteres, debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.";
+        return;
+    }
+    if (contrasena !== repetirContrasena) {
+        alerta.textContent = "*Las contraseñas no coinciden";
+        return;
+    }
+
+
+    const modal = document.querySelector('.modal');
+    modal.classList.add('modal--show');
+
+    // Cierra el modal automáticamente después de 2 segundos
+    setTimeout(() => {
+        modal.classList.remove('modal--show');
+    }, 2000);
+});
+
+const closeModal = document.querySelector('.modal_close');
+closeModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    const modal = document.querySelector('.modal');
+    modal.classList.remove('modal--show');
+});
+
+// Validaciones en tiempo real
+const correoInput = document.getElementById("correo");
+const contrasenaInput = document.getElementById("contrasena");
+const repetirContrasenaInput = document.getElementById("repetirContrasena");
+
+correoInput.addEventListener("input", function() {
+    const alerta = document.getElementById("alerta");
+    if (!correoInput.value.includes("@")) {
+        alerta.textContent = "*Ingrese una dirección de correo electrónico válida";
+    } else {
+        alerta.textContent = "";
+    }
+});
+
+contrasenaInput.addEventListener("input", function() {
+    const alerta = document.getElementById("alerta");
+    if (contrasenaInput.value.length < 8 || !/[A-Z]/.test(contrasenaInput.value) || !/[a-z]/.test(contrasenaInput.value) || !/[0-9]/.test(contrasenaInput.value) || !/[!@#$%^&*(),.?":{}|<>]/.test(contrasenaInput.value)) {
+        alerta.textContent = "Tu contraseña es insegura. Te recordamos que tiene que tener mínimo 8 caracteres, debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.";
+    } else {
+        alerta.textContent = "";
+    }
+});
+
+repetirContrasenaInput.addEventListener("input", function() {
+    const alerta = document.getElementById("alerta");
+    if (contrasenaInput.value !== repetirContrasenaInput.value) {
+        alerta.textContent = "*Las contraseñas no coinciden";
+    } else {
+        alerta.textContent = "";
+    }
+});
